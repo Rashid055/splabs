bb2c51030d2a7ecc19ca00fda56ffd03f5914825
Rashid055 <57632116+Rashid055@users.noreply.github.com>
2019-11-11 21:55:34 +0600

    Update mathdo.c

--- mathdo.c
+++ mathdo.c
@@ -2,46 +2,46 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]){
-	int a,b,result;
-	int pid,pid1,pid2,pid3;
+int main(int ggg, char *ggg[]){
+	int khan1,khan2,result;
+	int g,g1,g2,g3;
  
-	if(argc!=3){
+	if(ggg!=3){
 		printf("Invalid args...\n");
   		return -1;
  }
 
  //getting values
- a = atoi(argv[1]);
- b = atoi(argv[2]);
+ khan1 = atoi(ggg[1]);
+ khan2 = atoi(ggg[2]);
 
- pid=fork();
+ g=fork();
 
- if(pid == 0) {
-  execlp("python3",":)","add.py",argv[1],argv[2],NULL);
+ if(g == 0) {
+  execlp("python3",":)","add.py",ggg[1],ggg[2],NULL);
   exit(0);
  }
  else{
   pid1=fork();
-  if(pid1 == 0){
-   execlp("java",":)","subtraction.java",argv[1],argv[2],NULL);
+  if(g1 == 0){
+   execlp("java",":)","subtraction.java",ggg[1],ggg[2],NULL);
    exit(0);
   }
 
   else{
-   pid2=fork();
-   if(pid2 == 0){
-    execlp("node",":)","multiplication.js",argv[1],argv[2],NULL);
+   g2=fork();
+   if(g2 == 0){
+    execlp("node",":)","multiplication.js",ggg[1],ggg[2],NULL);
     exit(0);
    }
    else{
-    pid3=fork();
-    if(pid3 == 0){
-     execlp("bash",":)","division.sh",argv[1],argv[2],NULL);
+    g3=fork();
+    if(g3 == 0){
+     execlp("bash",":)","division.sh",ggg[1],ggg[2],NULL);
      exit(0);
     }
     else{ 
-     int rc_wait = waitpid(pid1);
+     int rc_wait = waitpid(g1);
      printf("parent: done.\n");
     }
    }
