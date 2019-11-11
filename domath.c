fe5c02f1362dd30b199a4e6a701ad87ddc10b629
Rashid055 <57632116+Rashid055@users.noreply.github.com>
2019-11-11 21:48:22 +0600

    Update domath.c

--- domath.c
+++ domath.c
@@ -4,30 +4,30 @@
 #include <wait.h>
 
 int main(int argc, char **argv){
-	int a = atoi(argv[1]);
-	int b = atoi(argv[2]);
+	int khan1 = atoi(argv[1]);
+	int khan2 = atoi(argv[2]);
 
-	pid_t p1 = fork();
-	if(p1==0){
-		printf("child1: %d+%d=%d\n", a, b, a+b);
+	pid_t pass1 = fork();
+	if(pass1==0){
+		printf("child1: %d+%d=%d\n", khan1, khan2, khan1+khan2);
 	}
-	waitpid(p1, 0, 0);
-	pid_t p2 = fork();
-	if(p2==0 && p1>0){
-		printf("child2: %d-%d=%d\n", a, b, a-b);
+	waitpid(pass2, 0, 0);
+	pid_t pass2 = fork();
+	if(pass2==0 && pass1>0){
+		printf("child2: %d-%d=%d\n", khan1, khan2, khan1-khan2);
 	}
-	waitpid(p2, 0, 0);
-	pid_t p3 = fork();
-	if(p2>0 && p3==0 && p1>0){
-		printf("child3: %d*%d=%d\n", a, b, a*b);
+	waitpid(pass2, 0, 0);
+	pid_t pass3 = fork();
+	if(pass2>0 && pass3==0 && pass1>0){
+		printf("child3: %d*%d=%d\n", khan1, khan2, khan1*khan2);
 	}
-	waitpid(p3, 0, 0);
-	pid_t p4 = fork();
-	if(p4==0 && p3>0 && p2>0 && p1>0){
-		printf("child4: %d/%d=%d\n", a, b, a/b);
+	waitpid(pass3, 0, 0);
+	pid_t pass4 = fork();
+	if(p4==0 && pass3>0 && pass2>0 && pass1>0){
+		printf("child4: %d/%d=%d\n", khan1, khan2, khan1/khan2);
 	}
-	waitpid(p4, 0, 0);
-	if(p1>0 && p2>0 && p3>0 && p4>0){
+	waitpid(pass4, 0, 0);
+	if(pass1>0 && pass2>0 && pass3>0 && pass4>0){
 		printf("parent: done.");
 	}
 }
